476444cbba7e525bbd1237fcba6b09d8a748bc1c
l3miage-adomobia <adil.adomo@gmail.com>
2024-01-23 17:12:01 +0100

    create header style and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HeaderComponent} from "./shared/components/header/header.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'connexion', component: HeaderComponent },
+  { path: 'panier', component: HeaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
